323c15374022c4b424cfbd97283708eec826c533
levunam30101999 <levunam30101999@gmail.com>
2020-12-01 14:33:24 +0700

    Delete field in entity

--- src/main/java/com/ltm/threetree/entity/Card.java
+++ src/main/java/com/ltm/threetree/entity/Card.java
@@ -1,18 +1,5 @@
 package com.ltm.threetree.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-@Document(collection = "tree.card")
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class Card {
-  @Id
-  private String id;
+
 }
--- src/main/java/com/ltm/threetree/entity/PlayerRound.java
+++ src/main/java/com/ltm/threetree/entity/PlayerRound.java
@@ -1,18 +1,4 @@
 package com.ltm.threetree.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-@Document(collection = "tree.round")
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class PlayerRound {
-  @Id
-  private String id;
 }
