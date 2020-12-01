e07d8b9e784d3af152ba1167a7ab374a84471555
levunam30101999 <levunam30101999@gmail.com>
2020-12-01 14:09:34 +0700

    Add annotation

--- src/main/java/com/ltm/threetree/service/UserService.java
+++ src/main/java/com/ltm/threetree/service/UserService.java
@@ -1,4 +1,9 @@
 package com.ltm.threetree.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class UserService {
 }
--- src/main/java/com/ltm/threetree/socket/SocketServer.java
+++ src/main/java/com/ltm/threetree/socket/SocketServer.java
@@ -1,4 +1,4 @@
 package com.ltm.threetree.socket;
 
-public class Server {
+public class SocketServer {
 }
