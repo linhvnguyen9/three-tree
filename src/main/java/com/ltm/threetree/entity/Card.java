c69ecca66b8df8404fec818e8a6b992239047516
phamphilongPTIT <phamphilong4101999@gmail.com>
2020-12-01 15:01:22 +0700

    feat: add code entity for project ọc kê ọc kê

--- src/main/java/com/ltm/threetree/entity/Card.java
+++ src/main/java/com/ltm/threetree/entity/Card.java
@@ -1,5 +1,24 @@
 package com.ltm.threetree.entity;
 
-public class Card {
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Builder
+public class Card implements Serializable {
+  private SuiteCard suiteCard;
+  private List<Integer> values = new ArrayList<>();
+
+  public Card(SuiteCard suiteCard) {
+    this.suiteCard = suiteCard;
+    this.values = Arrays.asList(1,2,3,4,5,6,7,8,9);
+  }
 }
--- src/main/java/com/ltm/threetree/entity/PlayerRound.java
+++ src/main/java/com/ltm/threetree/entity/PlayerRound.java
@@ -1,4 +1,21 @@
 package com.ltm.threetree.entity;
 
-public class PlayerRound {
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Builder
+public class PlayerRound implements Serializable {
+    @Id
+    private String id;
+    private Player player;
+    private Card card1;
+    private Card card2;
+    private Card card3;
 }
